23fdf5f3528b5e65b0712d41b961af3b9faffd2c
dangherve <dangherve@gmail.com>
2026-03-07 01:21:16 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/fry0815/MMM-SystemStats.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/MMM-SystemStats.git
+git push --force --set-upstream origin master
